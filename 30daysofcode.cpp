2de8df5b9f7b25d1f9203d7e0442de0aa345ce50
Anubhav Singh <66737188+anubhavv1998@users.noreply.github.com>
2021-01-19 22:56:55 +0530

    Update 30daysofcode.cpp

--- 30daysofcode.cpp
+++ 30daysofcode.cpp
@@ -18,3 +18,88 @@ Print 4 3 2 1. Each integer is separated by one space.
 
     return 0;
 }
+
+
+/*Task
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has  parameters:
+A string,first name .
+A string,last name .
+An integer,idNumber .
+An integer array (or vector) of test scores,scores .
+A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
+	
+};
+
+class Student :  public Person{
+   private:
+      vector<int> testScores;  
+   public:
+ Student(string firstName, string lastName, int id, vector<int> scores) : 
+ Person(firstName, lastName, id) {
+            this->testScores = scores;
+        }
+  
+      char calculate() {
+            int sum = 0;
+            int med;
+            char grade = 'T';
+   for (vector<int>::iterator it = testScores.begin() ; it != testScores.end(); ++it)
+                sum+=*it;
+            med = int(sum/testScores.size());
+ 
+            if (med>=90 && med<=100) { grade='O'; }
+            if (med>=80 && med<90)   { grade='E'; }
+            if (med>=70 && med<80)   { grade='A'; }
+            if (med>=55 && med<70)   { grade='P'; }
+            if (med>=40 && med<=55)  { grade='D'; }
+            
+            return grade;
+        }
+};
+
+
+/*Function Description
+Complete the factorial function in the editor below. Be sure to use recursion.
+
+factorial has the following paramter:
+
+int n: an integer
+*/
+
+// Complete the factorial function below.
+
+int factorial(int n) {
+    if(n<=1)
+    { 
+        return (1); 
+    } 
+    else
+        { 
+            return (n*(factorial(n-1))); 
+    }
+}
